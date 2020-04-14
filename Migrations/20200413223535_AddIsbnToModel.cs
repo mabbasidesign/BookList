10b627b61da001fc850564db9e6a3aaa3efeb4ad
Mohsen <mabbasidesign@yahoo.com>
2020-04-14 17:55:30 -0400

    AddValidaionToCreate

---
+++ Migrations/20200413223535_AddIsbnToModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookList.Migrations
+{
+    public partial class AddIsbnToModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Isbn",
+                table: "Book",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Isbn",
+                table: "Book");
+        }
+    }
+}
--- Model/Book.cs
+++ Model/Book.cs
@@ -14,5 +14,6 @@ namespace BookList.Model
         [Required]
         public string Name { get; set; }
         public string Author { get; set; }
+        public string Isbn { get; set; }
     }
 }
